2fc88c11c6f7e549b6fe9132ae4e780cd7d05615
Pragyesh Gupta <pragyesh.gupta@fogteams.com>
2022-05-17 20:09:58 +0530

    added Multiply

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+
+    Run|Debug
+    public static void main(String[] args){
+        int a =4 ;
+        int b = 2 ;
+        Systems.out.println("the product is " + (a*b))
+    }
+}
